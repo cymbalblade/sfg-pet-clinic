5a894084cc5948232eb99b8c521d21bd79e9f6c1
Timmay <timstack83@gmail.com>
2021-11-30 23:04:02 -0500

    fix package name to get unit test working.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,8 +1,9 @@
 package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class SfgPetClinicApplicationTests {
 
 	@Test
